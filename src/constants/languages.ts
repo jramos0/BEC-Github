449cd4a2b3671d2d1fd816befc769e89e1144aa7
jramos0 <jonathan007rm@gmail.com>
2025-04-02 13:43:31 -0600

    Languages constant

---
+++ src/constants/languages.ts
@@ -0,0 +1,18 @@
+export const supportedLanguages = [
+    "English",
+    "Čeština",
+    "Deutsch",
+    "Español",
+    "Eesti Keel",
+    "Suomi",
+    "Français",
+    "Bahasa Indonesia",
+    "Italiano",
+    "日本語",
+    "Norsk Bokmål",
+    "Português",
+    "Русский",
+    "Tiếng Việt",
+    "简体中文"
+  ];
+  
